cffb182b233a33d0a38b319b5be16261fb13ffda
Pablo Alonso Jimenez <palonsojimenez@outlook.com>
2020-07-01 15:28:55 +0200

    Added userName parameter

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,17 +1,23 @@
 package com.example.demo.model;
 
 public class User {
-    private String userId;
+    private final String userId;
+    private String userName;
 
-    public User(String userId) {
+    public User(String userId, String userName) {
         this.userId = userId;
+        this.userName = userName;
     }
 
     public String getUserId() {
         return userId;
     }
 
-    public void setUserId(String userId) {
-        this.userId = userId;
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
     }
 }
