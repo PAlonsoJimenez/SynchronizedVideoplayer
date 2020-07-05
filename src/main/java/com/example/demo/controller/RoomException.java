7270ae2e3ee3e69830335a014fbb405be5bc8160
Pablo Alonso Jimenez <palonsojimenez@outlook.com>
2020-07-05 19:14:47 +0200

    Added new static constant 'INVALID_ROOM_ID'. Added new constructor with additional message.

--- src/main/java/com/example/demo/controller/RoomException.java
+++ src/main/java/com/example/demo/controller/RoomException.java
@@ -2,22 +2,29 @@ package com.example.demo.controller;
 
 public class RoomException extends RuntimeException{
     public static int INVALID_USER_ID = 0;
+    public static int INVALID_ROOM_ID = 1;
 
     public RoomException(int type){
-        super(generateMessage(type));
+        super(generateMessage(type, ""));
     }
 
-    private static String generateMessage(int type) {
-        String exceptionMessage = getExceptionTypeMessage(type) + " exception. Unable to create new Room.";
+    public RoomException(int type, String additionalMessage){
+        super(generateMessage(type, additionalMessage));
+    }
+
+    private static String generateMessage(int type, String additionalMessage) {
+        String exceptionMessage = getExceptionTypeMessage(type) + " " + additionalMessage;
         return exceptionMessage;
     }
 
     private static String getExceptionTypeMessage(int type) {
         switch (type){
             case 0:
-                return "Invalid userId parameter";
+                return "Invalid userId parameter.";
+            case 1:
+                return "Invalid roomId parameter.";
             default:
-                return "Room Exception";
+                return "Room Exception.";
         }
     }
 }
