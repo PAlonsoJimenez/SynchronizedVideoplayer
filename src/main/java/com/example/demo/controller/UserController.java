094570bea92464dcbfa5b07576690ed62c62d87f
Pablo Alonso Jimenez <palonsojimenez@outlook.com>
2020-07-05 18:47:20 +0200

    Added constructor. Added LinkedLisit (as a placeholder) to keep record of userActives. Added isUserActive method

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,10 +1,25 @@
 package com.example.demo.controller;
 
+import java.util.LinkedList;
 import java.util.UUID;
 
 public class UserController {
+    //TODO: Remove this userList, it has to be elsewhere, like in a database.
+    private LinkedList<String> userActives;
+
+    public UserController(){
+        userActives = new LinkedList<>();
+    }
 
     public String createUserIdJsonFormat(){
-        return "{\"id\": \"" + UUID.randomUUID().toString() + "\"}";
+        String userID = UUID.randomUUID().toString();
+        userActives.add(userID);
+        return "{\"id\": \"" + userID + "\"}";
+    }
+
+    protected boolean isUserActive(String userId){
+        //TODO: This method
+        //TODO: this is only a placeholder
+        return userActives.contains(userId);
     }
 }
