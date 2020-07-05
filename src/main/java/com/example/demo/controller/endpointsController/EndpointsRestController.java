b0667d36e28f1036e3028c09fbe56a303902b199
Pablo Alonso Jimenez <palonsojimenez@outlook.com>
2020-07-05 18:52:21 +0200

    Added autowired attribute SimpMessagingTemplate to be able to call Stomp broadcasting endpoints without a client action. Added Rest endpoint 'joinRoom'

--- src/main/java/com/example/demo/controller/endpointsController/EndpointsRestController.java
+++ src/main/java/com/example/demo/controller/endpointsController/EndpointsRestController.java
@@ -3,6 +3,8 @@ package com.example.demo.controller.endpointsController;
 import com.example.demo.controller.RoomController;
 import com.example.demo.controller.UserController;
 import com.example.demo.model.Room;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,15 +14,27 @@ import org.springframework.web.bind.annotation.RestController;
 public class EndpointsRestController {
     UserController userController = new UserController();
     RoomController roomController = new RoomController();
+    @Autowired
+    private SimpMessagingTemplate template;
 
     @GetMapping(value = "/getUserId", produces = "application/json")
     public String getUserId(){
         return userController.createUserIdJsonFormat();
     }
 
-    @PostMapping(value = "/createRoom" , produces = "application/json")
+    @PostMapping(value = "/createRoom", produces = "application/json")
     public Room createRoom(@RequestParam(name = "userId") String userId,
                            @RequestParam(name = "userName") String userName){
         return roomController.createRoom(userId, userName);
+    }
+
+    @PostMapping(value = "/joinRoom", produces = "text/plain")
+    public String joinRoom(@RequestParam(name = "userId") String userId,
+                           @RequestParam(name = "userName") String userName,
+                           @RequestParam(name = "roomId") String roomId){
+        boolean joined = roomController.joinRoom(userId, userName, roomId, userController, template);
+        //I think, due to the error throwed inside the roomController, is not possible to return "Unable to Join",
+        //which is not a bad thing. (just wanted to remember it)
+        return ((joined) ? "User Joined" : "Unable to Join");
     }
 }
