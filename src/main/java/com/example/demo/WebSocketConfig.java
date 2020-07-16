5498a6d545d2ce9965ee4fa476ce6849b00e66d0
Pablo Alonso Jimenez <palonsojimenez@outlook.com>
2020-07-17 00:07:07 +0200

    Added ChannelInterceptor

--- src/main/java/com/example/demo/WebSocketConfig.java
+++ src/main/java/com/example/demo/WebSocketConfig.java
@@ -1,6 +1,8 @@
 package com.example.demo;
 
+import com.example.demo.controller.MessagesInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -21,4 +23,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.addEndpoint("/room/{roomId}", "/roomInfo/{roomId}");
         registry.addEndpoint("/room/{roomId}", "/roomInfo/{roomId}").withSockJS();
     }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(new MessagesInterceptor());
+    }
+
 }
