b8fcf1d157e1f3603d4ed7ade8a7be76ba30b040
Pablo Alonso Jimenez <palonsojimenez@outlook.com>
2020-07-16 22:58:59 +0200

    Moved user validations here. Added Database object (currently using static methods)

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,25 +1,39 @@
 package com.example.demo.controller;
 
-import java.util.LinkedList;
+import com.example.demo.persistence.DatabaseManager;
+
 import java.util.UUID;
+import java.util.regex.Pattern;
 
 public class UserController {
-    //TODO: Remove this userList, it has to be elsewhere, like in a database.
-    private LinkedList<String> userActives;
+    //TODO: Add DatabaseManager object
+    //DatabaseManager databaseManager;
 
+    /*
     public UserController(){
-        userActives = new LinkedList<>();
+        databaseManager = new DatabaseManager();
     }
+    */
 
     public String createUserIdJsonFormat(){
-        String userID = UUID.randomUUID().toString();
-        userActives.add(userID);
-        return "{\"id\": \"" + userID + "\"}";
+        String userId = UUID.randomUUID().toString();
+        DatabaseManager.addActiveUser(userId);
+        return "{\"id\": \"" + userId + "\"}";
     }
 
     protected boolean isUserActive(String userId){
-        //TODO: This method
-        //TODO: this is only a placeholder
-        return userActives.contains(userId);
+        return DatabaseManager.isUserActive(userId);
+    }
+
+    protected boolean isUserIdValid(String userId) {
+        Pattern p = Pattern.compile("[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$");
+        return p.matcher(userId).matches();
+    }
+
+    protected String validateUserName(String userName) {
+        userName = userName.trim();
+        if(userName.isEmpty()) userName = "XxXlittle_weeperxXx";
+        if(userName.length() > 20) userName = userName.substring(0,20);
+        return userName;
     }
 }
