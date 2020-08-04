b6ad2cb22fc6bee8bcfc13c74a3220bd0645ab78
Pablo Alonso Jimenez <palonsojimenez@outlook.com>
2020-08-04 16:54:03 +0200

    Now returning an array of users instaed of a single user. Added a new constructor with a set of user as a parameter

--- src/main/java/com/example/demo/model/RoomInfoMessage.java
+++ src/main/java/com/example/demo/model/RoomInfoMessage.java
@@ -1,24 +1,34 @@
 package com.example.demo.model;
 
+import java.util.ArrayList;
+import java.util.Set;
+
 public class RoomInfoMessage {
     //TODO: For the user do something as publicId and privateID
     public enum Action{
-        JOINING, LEAVING
+        JOINING, LEAVING, FULL_ROOM_INFO
     }
 
+    private final ArrayList<User> users;
     private final Action action;
-    private final User user;
 
     public RoomInfoMessage(Action action, User user){
+        users = new ArrayList<>();
         this.action = action;
-        this.user = user;
+        users.add(user);
+    }
+
+    public RoomInfoMessage(Set<User> users){
+        this.users = new ArrayList<>();
+        this.users.addAll(users);
+        action = Action.FULL_ROOM_INFO;
     }
 
     public Action getAction() {
         return action;
     }
 
-    public User getUser() {
-        return user;
+    public ArrayList<User> getUsers() {
+        return users;
     }
 }
