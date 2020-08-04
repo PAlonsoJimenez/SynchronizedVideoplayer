bb077bf0ccf0797a2d78a6866d1de6fd3776ab38
Pablo Alonso Jimenez <palonsojimenez@outlook.com>
2020-08-04 16:47:16 +0200

    First commit

---
+++ src/main/java/com/example/demo/controller/SubscriptionController.java
@@ -0,0 +1,62 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.RoomInfoMessage;
+import com.example.demo.model.User;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.web.socket.messaging.SessionSubscribeEvent;
+
+public class SubscriptionController {
+    private final RoomController roomController;
+
+    public SubscriptionController(){
+        roomController = new RoomController();
+    }
+
+    public void afterSubscribeEventHandler (SessionSubscribeEvent subscribeEvent, SimpMessagingTemplate template){
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(subscribeEvent.getMessage());
+        String destination = headerAccessor.getDestination();
+        if(destination.contains("roomInfoController")){
+            RoomInfoMessage allRoomInfoMessage = getAllRoomInfoMessage(destination);
+            if(allRoomInfoMessage != null){
+                String userSessionId = headerAccessor.getUser().getName();
+                template.convertAndSendToUser(userSessionId, "/queue/reply", allRoomInfoMessage);
+            }
+            //At this point, the user (userId and userName) and room have been validated by the MessageInterceptor
+            addUserToRoom(headerAccessor);
+
+            String userId = headerAccessor.getFirstNativeHeader("userId");
+            String userName = headerAccessor.getFirstNativeHeader("userName");
+            template.convertAndSend(destination, new RoomInfoMessage(RoomInfoMessage.Action.JOINING, new User(userId, userName)));
+        }
+    }
+
+    private RoomInfoMessage getAllRoomInfoMessage(String destination) {
+        //TODO magic number...
+        String[] fullEndpointDestination = destination.split("/");
+        if(fullEndpointDestination.length != 4){
+            //TODO log problem...
+            return null;
+        }
+        String roomId = fullEndpointDestination[3];
+        return roomController.getRoomMembersInfo(roomId);
+    }
+
+    private void addUserToRoom(StompHeaderAccessor headerAccessor) {
+        //At this point, the user (userId and userName) and room have been validated by the MessageInterceptor
+        String destination = headerAccessor.getDestination();
+        String[] destinationPages = destination.split("/");
+        //Todo: Magic number
+        if(destinationPages.length != 4){
+            //TODO: log error
+            return;
+        }
+
+        String userId = headerAccessor.getFirstNativeHeader("userId");
+        String userName = headerAccessor.getFirstNativeHeader("userName");
+        //TODO: magic number...
+        String roomId = destinationPages[3];
+
+        roomController.joinRoom(userId, userName, roomId, new UserController());
+    }
+}
