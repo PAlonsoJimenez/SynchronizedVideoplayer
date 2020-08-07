ac544f873fc58fbfe17eed6d7837126d1ed42c0d
Pablo Alonso Jimenez <palonsojimenez@outlook.com>
2020-08-07 18:33:49 +0200

    Added list of subscribers to each possible room topic

--- src/main/java/com/example/demo/model/Room.java
+++ src/main/java/com/example/demo/model/Room.java
@@ -1,5 +1,6 @@
 package com.example.demo.model;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.UUID;
 
@@ -7,11 +8,15 @@ public class Room {
     private final User creator;
     private final HashSet<User> members;
     private final String roomId;
+    private final ArrayList<String> videoControllerSubscribers;
+    private final ArrayList<String> roomInfoSubscribers;
 
     public Room(User creator) {
         this.creator = creator;
         members = new HashSet<>();
         roomId = createRoomId();
+        videoControllerSubscribers = new ArrayList<>();
+        roomInfoSubscribers = new ArrayList<>();
     }
 
     public boolean addMember(User newMember){
@@ -32,5 +37,23 @@ public class Room {
 
     private String createRoomId() {
         return UUID.randomUUID().toString();
+    }
+
+    public boolean addVideoControllerSubscriber(String connectionId){
+        if(videoControllerSubscribers.contains(connectionId)){
+            return false;
+        }else{
+            videoControllerSubscribers.add(connectionId);
+            return true;
+        }
+    }
+
+    public boolean addRoomInfoSubscriber(String connectionId){
+        if(roomInfoSubscribers.contains(connectionId)){
+            return false;
+        }else{
+            roomInfoSubscribers.add(connectionId);
+            return true;
+        }
     }
 }
