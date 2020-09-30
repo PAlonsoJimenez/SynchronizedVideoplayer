fb2f5c6ae552dbb88107061186dece0e3c80b644
Pablo Alonso Jimenez <palonsojimenez@outlook.com>
2020-09-30 02:35:10 +0200

    Added setUserConnectionId method, and made protected methods public. Now getting the user from the DatabaseManager rather than asking if the user is active.

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.User;
 import com.example.demo.persistence.DatabaseManager;
 
 import java.util.UUID;
@@ -17,23 +18,30 @@ public class UserController {
 
     public String createUserIdJsonFormat(){
         String userId = UUID.randomUUID().toString();
-        DatabaseManager.addActiveUser(userId);
+        //TODO: Somehow update the userName when the user changes it.
+        User newUser = new User(userId, "Anonymous");
+        DatabaseManager.addActiveUser(newUser);
         return "{\"id\": \"" + userId + "\"}";
     }
 
-    protected boolean isUserActive(String userId){
-        return DatabaseManager.isUserActive(userId);
+    public boolean isUserActive(String userId){
+        User user = DatabaseManager.getUser(userId);
+        return (user != null);
     }
 
-    protected boolean isUserIdValid(String userId) {
+    public boolean isUserIdValid(String userId) {
         Pattern p = Pattern.compile("[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$");
         return p.matcher(userId).matches();
     }
 
-    protected String validateUserName(String userName) {
+    public String validateUserName(String userName) {
         userName = userName.trim();
         if(userName.isEmpty()) userName = "XxXlittle_weeperxXx";
         if(userName.length() > 20) userName = userName.substring(0,20);
         return userName;
+    }
+
+    public boolean setUserConnectionId(String userId, String connectionId){
+        return DatabaseManager.setUserConnectionId(userId, connectionId);
     }
 }
