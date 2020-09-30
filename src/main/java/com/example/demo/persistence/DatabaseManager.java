fc96ac6ec48bdb1063807c662fb4f42b6b1547bd
Pablo Alonso Jimenez <palonsojimenez@outlook.com>
2020-09-30 02:51:34 +0200

    Now adding an User object to the room, if and only if its not already in the room. Added setUserConnectionId method

--- src/main/java/com/example/demo/persistence/DatabaseManager.java
+++ src/main/java/com/example/demo/persistence/DatabaseManager.java
@@ -8,21 +8,24 @@ import java.util.LinkedList;
 public class DatabaseManager {
     //TODO: This is almost a mock class for now. Write this class using a database
 
-    private final static LinkedList<String> activeUsers = new LinkedList<>();
+    private final static LinkedList<User> activeUsers = new LinkedList<>();
     //TODO: Close a Room when there is no user in it.
     private final static LinkedList<Room> openedRooms = new LinkedList<>();
 
 
-    public static void addActiveUser(String userId){
-        activeUsers.add(userId);
+    public static void addActiveUser(User user){
+        activeUsers.add(user);
     }
 
     public static void addCreatedRoom(Room room){
         openedRooms.add(room);
     }
 
-    public static boolean isUserActive(String userId){
-        return activeUsers.contains(userId);
+    public static User getUser(String userId){
+        return (activeUsers.stream()
+                .filter(user -> user.getUserId().equals(userId))
+                .findFirst()
+                .orElse(null));
     }
 
     public static Room getRoom(String roomId){
@@ -33,9 +36,34 @@ public class DatabaseManager {
     }
 
     public static boolean addUserToRoom(String roomId, User user){
-        //TODO: add user if and only if is not already in the room
         //TODO: Improve this with a single query to the database from here
-        getRoom(roomId).addMember(user);
+        Room room = getRoom(roomId);
+        if(room == null) return false;
+
+        Boolean alreadyContainsUser = false;
+        for (User roomUser : room.getMembers()) {
+            if(roomUser.getUserId().equals(user.getUserId())){
+                alreadyContainsUser = true;
+                break;
+            }
+        }
+
+        if(alreadyContainsUser) return false;
+
+        room.addMember(user);
         return true;
+    }
+
+    public static boolean setUserConnectionId(String userId, String connectionId) {
+        boolean connectionIdSet = false;
+        for (User user : activeUsers) {
+            if(user.getUserId().equals(userId)){
+                user.setConnectionId(connectionId);
+                connectionIdSet = true;
+                break;
+            }
+        }
+
+        return connectionIdSet;
     }
 }
