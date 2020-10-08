1f3576599d4977888c6aa23d1e93f4bc534b9499
Pablo Alonso Jimenez <palonsojimenez@outlook.com>
2020-10-08 03:20:38 +0200

    Added attributes 'videoControllerRoomIds' and 'roomInfoId', their getter, and methods to add or remove ids from these lists

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,14 +1,48 @@
 package com.example.demo.model;
 
+import java.util.ArrayList;
+
 public class User {
+    private final ArrayList<String> videoControllerRoomIds;
+    private final ArrayList<String> roomInfoRoomIds;
     private final String userId;
     private String userName;
     private String connectionId;
 
     public User(String userId, String userName) {
+        videoControllerRoomIds = new ArrayList<>();
+        roomInfoRoomIds = new ArrayList<>();
         this.userId = userId;
         this.userName = userName;
-        this.connectionId = null;
+        connectionId = null;
+    }
+
+    public void addVideoControllerRoomId(String roomId){
+        if(!videoControllerRoomIds.contains(roomId))
+            videoControllerRoomIds.add(roomId);
+    }
+
+    public void removeVideoControllerRoomId(String roomId){
+        if(videoControllerRoomIds.contains(roomId))
+            videoControllerRoomIds.remove(roomId);
+    }
+
+    public ArrayList<String> getVideoControllerRoomIds(){
+        return videoControllerRoomIds;
+    }
+
+    public void addRoomInfoRoomId(String roomId){
+        if(!roomInfoRoomIds.contains(roomId))
+            roomInfoRoomIds.add(roomId);
+    }
+
+    public void removeRoomInfoRoomId(String roomId){
+        if(roomInfoRoomIds.contains(roomId))
+            roomInfoRoomIds.remove(roomId);
+    }
+
+    public ArrayList<String> getRoomInfoRoomIds(){
+        return roomInfoRoomIds;
     }
 
     public String getUserId() {
